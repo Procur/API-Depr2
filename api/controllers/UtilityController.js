674093b101d6fa5e57063135979496493a8a576f
Will <will@willricketts.com>
2014-08-01 16:05:30 -0400

    added utility controller. deleted default view

---
+++ api/controllers/UtilityController.js
@@ -0,0 +1,11 @@
+/**
+ * UtilityController
+ *
+ * @description :: Server-side logic for managing utilities
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
