405e2f7586d140fa92b50c2361e0845f7f9b8ade
Will <will@willricketts.com>
2014-08-01 19:10:48 -0400

    added message model attributes

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -6,6 +6,32 @@
  */
 
 module.exports = {
-	
-};
 
+	attributes: {
+
+		user: {
+			type: 'string',
+			required: true
+		},
+
+		recipient: {
+			type: 'string',
+			required: true,
+		},
+
+		content: {
+			type: 'text',
+			required: true
+		},
+
+		parent: {
+			type: 'string'
+		},
+
+		opened: {
+			type: 'boolean',
+			required: true
+		}
+	}
+
+};
