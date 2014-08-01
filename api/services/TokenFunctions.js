3d81e89ff03ae1d41f65a136ac52d592e1cd53fc
Will <will@willricketts.com>
2014-08-01 18:36:13 -0400

    added TokenFunctions.js helpers

---
+++ api/services/TokenFunctions.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  validateByUserId: function(apitoken, userId, callback){
+
+  },
+
+  validateByUserEmail: function(apitoken, email, callback){
+
+  },
+
+  findByUserEmail: function(email, callback){
+
+  },
+
+  findByUserId: function(id, callback){
+
+  },
+}
