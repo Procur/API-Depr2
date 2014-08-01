0547d69833eae3f0c68376f063b48ef472340053
Will <will@willricketts.com>
2014-08-01 19:07:56 -0400

    added Rfx model and controller

---
+++ api/controllers/RfxController.js
@@ -0,0 +1,11 @@
+/**
+ * RfxController
+ *
+ * @description :: Server-side logic for managing rfxes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Rfx.js
@@ -0,0 +1,33 @@
+/**
+* Rfx.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    user: {
+      type: 'string',
+      required: true
+    },
+
+    type: {
+      type: 'string',
+      required: true
+    },
+
+    recipient: {
+      type: 'string',
+      required: 'true'
+    },
+
+    content: {
+      type: 'text',
+      required: true
+    }
+
+  }
+};
