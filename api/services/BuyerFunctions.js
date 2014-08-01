a6fe59d64d1ecabf5b49a8595a47d94dd3524f3f
Will <will@willricketts.com>
2014-08-01 18:39:57 -0400

    added CompanyFunctions and BuyerFunctions.js helpers

---
+++ api/services/BuyerFunctions.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  findBySiblingId: function(supplierId, callback){
+
+  },
+
+  findByUserId: function(userId, callback){
+
+  },
+
+  findByUserEmail: function(email, callback){
+
+  },
+
+  findByCompanyId: function(companyId, callback){
+
+  },
+
+  findByCompanyHandle: function(handle, callback){
+    
+  }
+}
---
+++ api/services/CompanyFunctions.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  findByUserId: function(userId, callback){
+
+  },
+
+  findByUserEmail: function(email, callback){
+
+  }
+}
