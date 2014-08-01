5a829009084bbc821f51e0e406a7aaea656dc754
Will <will@willricketts.com>
2014-08-01 16:01:30 -0400

    Added models and controllers for buyer, supplier, company, and user

---
+++ api/controllers/CompanyController.js
@@ -0,0 +1,11 @@
+/**
+ * CompanyController
+ *
+ * @description :: Server-side logic for managing companies
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
