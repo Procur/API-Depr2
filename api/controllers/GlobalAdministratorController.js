1f6e032121917879ce02477101e8601fb611ae90
Will <will@willricketts.com>
2014-08-02 09:00:59 -0400

    added globalAdministrator controller

---
+++ api/controllers/GlobalAdministratorController.js
@@ -0,0 +1,26 @@
+/**
+ * GlobalAdministratorController
+ *
+ * @description :: Server-side logic for managing globaladministrators
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+    index: function(req, res){
+
+    },
+
+    show: function(req,res){
+
+    },
+
+    create: function(req, res){
+
+    },
+
+    demote: function(req, res){
+
+    }
+};
+
