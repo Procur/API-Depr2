5100baf8d9ce43fbeb26545434f972aaccc47604
Will <will@willricketts.com>
2014-08-01 22:31:37 -0400

    added log model content

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -6,6 +6,40 @@
  */
 
 module.exports = {
-	
-};
 
+	index: function(req, res){
+
+	},
+
+	show: function(req, res){
+
+	},
+
+	create: function(req, res){
+
+	},
+
+	destroy: function(req, res){
+
+	},
+
+	addNote: function(req, res){
+
+	},
+
+	updateNote: function(req, res){
+
+	},
+
+	deleteNote: function(req, res){
+
+	},
+
+	setFlag: function(req, res){
+
+	},
+
+	unsetFlag: function(req, res){
+
+	}
+};
