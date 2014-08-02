b3d10febf6f47f63e8a77bc537afe7716a96b9d1
Will <will@willricketts.com>
2014-08-01 23:07:59 -0400

    added UserFunctions

--- api/services/UserFunctions.js
+++ api/services/UserFunctions.js
@@ -1,22 +1,30 @@
 module.exports = {
 
-  findByEmail: function(apitoken, email, callback) {
-
+  findByEmail: function(email, callback) {
+    User.findOne({ email: email }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      callback(user);
+    })
   },
 
   findByApiToken: function(apitoken, callback){
-
+    ApiToken.findOne({ token: apitoken }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      callback(user);
+    });
   },
 
   findByCompany: function(companyId, callback){
-
-  },
-
-  findByBuyer: function(buyerId, callback){
-
-  },
-
-  findBySupplier: function(supplierId, callback){
-    
+    Company.findOne({ id: companyId }, function(err, company){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(company, res);
+      User.findOne({ id: company.user }, function(err, user){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(user, res);
+        callback(user);
+      })
+    })
   }
 };
