58048a3e9050e9a2d2627cb05520267a8f8493c5
Will <will@willricketts.com>
2014-08-02 17:50:57 -0400

    added UserPreference model and controller

---
+++ api/controllers/UserPreferenceController.js
@@ -0,0 +1,26 @@
+/**
+ * UserPreferenceController
+ *
+ * @description :: Server-side logic for managing Userpreferences
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  index: function(req, res) {
+
+  },
+
+  show: function(req, res) {
+
+  },
+
+  create: function(req, res) {
+
+  },
+
+  update: function(req, res) {
+
+  }
+};
+
