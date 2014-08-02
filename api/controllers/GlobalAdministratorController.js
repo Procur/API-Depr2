a8ba4cb3a63df86fac376b2680e638da7249faae
Will <will@willricketts.com>
2014-08-02 17:03:18 -0400

    fleshed out GlobalAdministrator Controller

--- api/controllers/GlobalAdministratorController.js
+++ api/controllers/GlobalAdministratorController.js
@@ -8,19 +8,46 @@
 module.exports = {
 
   index: function (req, res) {
-
+    User.find({ globalAdministrator: true }).exec(function(err, users){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(users, res);
+      return res.json(200, users);
+    });
   },
 
   show: function (req, res) {
-
+    var p = req.params;
+    User.findOne({ id: p.id, globalAdministrator: true}, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      return res.json(200, user);
+    });
   },
 
   makeAdmin: function (req, res) {
-
+    var p = req.params;
+    User.findOne({ id: p.id }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      User.update(user, { globalAdministrator: true }, function(err, user){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(user, res);
+        return res.json(200, user);
+      });
+    });
   },
 
   demote: function (req, res) {
-
+    var p = req.params;
+    User.findOne({ id: p.id, globalAdministrator: true}, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      User.update(user, { globalAdministrator: false }, function(err, user){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(user, res);
+        return res.json(202, user);
+      });
+    });
   }
 };
 
