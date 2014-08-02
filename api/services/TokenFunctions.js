33802240a2008bcee48f26bc92acdc1dcbb349ce
Will <will@willricketts.com>
2014-08-01 22:47:39 -0400

    added token functions

--- api/services/TokenFunctions.js
+++ api/services/TokenFunctions.js
@@ -1,18 +1,68 @@
 module.exports = {
 
-  validateByUserId: function(apitoken, userId, callback){
-
+  validateByUserId: function(apitoken, userId, res, callback){
+    User.findOne({ id: userId }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      ApiToken.findOne({ user: user.id }, function(err, token){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(token, res);
+        if(apitoken == token){
+          callback(token);
+        }
+        else{
+          Log.create({ content: 'An invalid token was used' }, function(err, log){
+            if(log !== undefined){
+              res.send(500, 'Invalid API token');
+            }
+          });
+        }
+      });
+    });
   },
 
   validateByUserEmail: function(apitoken, email, callback){
-
+    User.findOne({ email: email }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      ApiToken.findOne({ user: user.id }, function(err, token){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(token, res);
+        if(apitoken == token){
+          callback(token);
+        }
+        else {
+          Log.create({ content: 'An invalid token was used' }, function(err, log){
+            if(log !== undefined){
+              res.send(500, 'Invalid API token');
+            }
+          });
+        }
+      });
+    });
   },
 
   findByUserEmail: function(email, callback){
-
+    User.findOne({ email: email }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      ApiToken.findOne({ user: user.id }, function(err, token){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(token, res);
+        callback(token);
+      });
+    });
   },
 
   findByUserId: function(id, callback){
-
+    User.findOne({ id: id }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      ApiToken.findOne({ user: user.id }, function(err, token){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(token, res);
+        callback(token);
+      });
+    });
   }
 };
