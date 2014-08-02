ab4bd9a1d5e607ff5a3c2ecebd23be99cfd07cbd
Will <will@willricketts.com>
2014-08-02 17:11:58 -0400

    adding message helpers

---
+++ api/services/MessageFunctions.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  findOneById: function(messageId, callback){
+    Message.findOne({ id: messageId}, function(err, message){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(message, res);
+      callback(message);
+    });
+  },
+
+  findAllByUserId: function(userId, callback){
+    User.findOne({ id: userId }, function(err, user){
+      errorHandler.serverError(err, res);
+      errorHandler.nullCollection(user, res);
+      Message.find({ user: user.id }).sort({ createdAt: 'desc' }).exec(function(err, messages){
+        errorHandler.serverError(err, res);
+        errorHandler.nullCollection(messages, res);
+        callback(messages);
+      });
+    });
+  }
+}
