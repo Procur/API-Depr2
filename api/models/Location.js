5cf44fb33bdc4e6921bf5631382ee618eb2950e2
Will <will@willricketts.com>
2014-08-04 14:03:47 -0400

    added location model and controller

---
+++ api/models/Location.js
@@ -0,0 +1,29 @@
+/**
+* Location.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    company: {
+      type: 'string'
+    },
+
+    buyer: {
+      type: 'string'
+    },
+
+    supplier: {
+      type: 'string'
+    },
+
+    isHq: {
+      type: 'string'
+    }
+  }
+};
+
